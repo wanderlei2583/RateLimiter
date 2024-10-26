730543ed4c3b670720b0424a5973bcf5add8ff59
Wanderlei Pereira <devwanderlei@gmail.com>
2024-10-26 20:17:13 -0300

    ajustado as configs do middleware

---
+++ middleware.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+)
+
+type RateLimitMiddleware struct {
+	limiter *RateLimiter
+}
+
+func NewRateLimiterMiddleware(limiter *RateLimiter) *RateLimitMiddleware {
+	return &RateLimitMiddleware{limiter: limiter}
+}
+
+func (m *RateLimitMiddleware) Limit(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var allowed bool
+		var err error
+
+		token := r.Header.Get("API_KEY")
+		if token != "" {
+			allowed, err = m.limiter.IsAllowed(token, "token")
+		} else {
+			ip := r.RemoteAddr
+			if realIP := r.Header.Get("X-Real-IP"); realIP != "" {
+				ip = realIP
+			} else if forwardedFor := r.Header.Get("X-Forwarded-For"); forwardedFor != "" {
+				ip = strings.Split(forwardedFor, ",")[0]
+			}
+
+			allowed, err = m.limiter.IsAllowed(ip, "ip")
+		}
+
+		if err != nil {
+			http.Error(
+				w,
+				"Internal Server Error",
+				http.StatusInternalServerError,
+			)
+			return
+		}
+
+		if !allowed {
+			http.Error(
+				w,
+				"you have reached the maximum number of requests or actions allowed within a certain time frame",
+				http.StatusTooManyRequests,
+			)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
