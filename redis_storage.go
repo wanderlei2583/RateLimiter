5e8539421a5d4f419a660513f0392face7aee473
Wanderlei Pereira <devwanderlei@gmail.com>
2024-10-26 20:01:08 -0300

    ajustando as configs do redis storage

---
+++ redis_storage.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+type RedisConfig struct {
+	Host string
+	Port string
+}
+
+type RedisStorage struct {
+	client *redis.Client
+}
+
+func NewRedisStorage(config *RedisConfig) *RedisStorage {
+	client := redis.NewClient(&redis.Options{
+		Addr: config.Host + ":" + config.Port,
+	})
+
+	return &RedisStorage{client: client}
+}
+
+func (rs *RedisStorage) Increment(
+	key string,
+	expiry time.Duration,
+) (int error) {
+	ctx := context.Background()
+
+	pipe := rs.client.Pipeline()
+	incr := pipe.Incr(ctx, key)
+	pipe.Expire(ctx, key, expiry)
+
+	_, err := pipe.Exec(ctx)
+	if err != nil {
+		return 0, err
+	}
+
+	return int(incr.Val()), nil
+}
