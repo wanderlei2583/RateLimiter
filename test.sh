e97b81037023539ea6b9cf6b6be82329964e9b00
Wanderlei Pereira <devwanderlei@gmail.com>
2024-10-27 11:36:47 -0300

    adicionado saidas mais abrangentes dos testes de IP e Token

--- test.sh
+++ test.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+
+#  Teste de IP do rate limiter
+echo "Testando IP rate limiting..."
 for i in {1..6}; do
-    curl http://localhost:8080/
-    echo
+    echo "Request $i:"
+    curl -i http://localhost:8080/
+    echo -e "\n"
+    sleep 0.1
 done
 
+echo "Aguarde o teste reiniciar..."
+sleep 2
+
+#  Teste de token do rate limiter
+echo "Testando token rate limiting..."
 for i in {1..11}; do
-    curl -H "API_KEY: test_token" http://localhost:8080/
-    echo
+    echo "Request $i:"
+    curl -i -H "API_KEY: abc123" http://localhost:8080/
+    echo -e "\n"
+    sleep 0.1
 done
